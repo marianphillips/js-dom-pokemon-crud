eb75806dc816f0f00ea8328b1cb6348365cab0b8
marianphillips <79763160+marianphillips@users.noreply.github.com>
2022-02-16 16:19:02 +0000

    core completed although not my finest work

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const pokeForm = document.querySelector(".poke-form");
+const pokeDeleteForm = document.querySelector(".poke-delete-form");
 const pokeList = document.querySelector(".poke-list");
 
 function addPokemon(pokemon) {
@@ -13,6 +14,43 @@ function addPokemon(pokemon) {
 
   liEl.append(imgEl, h2El);
   pokeList.append(liEl);
+
+  const likeImg = document.createElement("img");
+  likeImg.setAttribute('src', 'https://filmdaily.co/wp-content/uploads/2021/07/fbl-01.jpg')
+  likeImg.setAttribute('class', 'like-image')
+
+  if(pokemon.liked === true) {
+    liEl.append(likeImg)
+  }
+
+  liEl.addEventListener("click", function (event) {
+    if(pokemon.liked === true) {
+      fetch(`http://localhost:3000/pokemons/${pokemon.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({liked: false})
+      })
+        .then(res =>  res.json())
+        .then(pokemon => console.log(pokemon));
+        likeImg.remove()  
+    }
+    else {
+      
+      fetch(`http://localhost:3000/pokemons/${pokemon.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({liked: true})
+      })
+        .then(res =>  res.json())
+        .then(pokemon => console.log(pokemon));  
+        liEl.append(likeImg)   
+    }
+
+})
 }
 
 function addPokemons(pokemons) {
@@ -28,28 +66,46 @@ function listenToAddPokemonForm() {
     };
 
     // CREATE
-    // fetch("http://localhost:3000/pokemons", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   },
-    //   body: JSON.stringify(pokemon)
-    // })
-    //   .then(res =>  res.json())
-    //   .then(pokemon => addPokemon(pokemon));
-    //   });
+    fetch("http://localhost:3000/pokemons", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(pokemon)
+    })
+      .then(res =>  res.json())
+      .then(pokemon => addPokemon(pokemon));
+    
 
     pokeForm.reset();
   });
 }
 
+  pokeDeleteForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+ 
+    // DELETE
+    fetch(`http://localhost:3000/pokemons/${pokeDeleteForm.id.value}`, {
+      method: 'DELETE'})
+      .then(res =>  res.json())
+      .then(pokemon => console.log(pokemon));
+    
+
+    pokeDeleteForm.reset();
+  });
+
+
+
+
 function init() {
   listenToAddPokemonForm();
-
+  // listenToLikePokemon();
   // READ
-  // fetch("http://localhost:3000/pokemons")
-  //   .then(res => res.json());
-  //   .then(pokemons => addPokemons(pokemons));
+  fetch("http://localhost:3000/pokemons")
+    .then(res => res.json())
+    .then(pokemons => addPokemons(pokemons));
 }
 
 init();
+
+
